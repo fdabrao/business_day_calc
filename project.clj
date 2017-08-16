b7bd852a727d2f47fd0233939c8df722c18b8073
FERNANDO ABRÃO <fdabrao@gmail.com>
2017-08-16 00:09:23 -0300

    Configure site url

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject business-date-calc "1.0.0"
+  :description "Calculate end date for given working hours."
+  :url "http://github.com/fabrao/business_day_calc"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-time "0.14.0"]])
