1054c45d404ca23231f9963f28c73d5e61fd8d70
FERNANDO ABRÃO <fdabrao@gmail.com>
2017-08-16 01:24:42 -0300

    Include group id

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject business-date-calc "1.0.0"
+(defproject fabrao/business-date-calc "1.0.0"
   :description "Calculate end date for given working hours."
   :url "http://github.com/fabrao/business_day_calc"
   :dependencies [[org.clojure/clojure "1.8.0"]
